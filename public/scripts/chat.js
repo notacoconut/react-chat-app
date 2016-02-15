767106b2b064892379b8697817e8ffc6e82f351c
Jered Spears <jeredspears.c@gmail.com>
2016-02-14 18:26:52 -0600

    added message sumbit functionality

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -8,22 +8,73 @@
       //online status
   //InputBox
 var Chat = React.createClass({
+  getInitialState: function(){
+    return { userData: [], messageData: [] };
+  },
+  loadMessagesFromServer: function(){
+    console.log("get");
+    $.ajax({
+      url: this.props.messageURL,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({messageData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error(this.props.messageURL, status, err.toString());
+      }.bind(this)
+    });
+  },
+  // loadUsersFromServer: function(){
+  //   $.ajax({
+  //     url: this.props.userURL
+  //   });
+  // },
+  handleMessageSubmit: function(message){
+    var messages = this.state.messageData;
+
+    message.id = Date.now();
+    var newMessages = messages.concat([message]);
+    this.setState({messageData: newMessages});
+    $.ajax({
+      url: this.props.messageURL,
+      dataType: 'json',
+      type: 'POST',
+      data: message,
+      success: function(data){
+        this.setState({messageData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({messageData: messages});
+        console.error(this.props.messageURL, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function(){
+    this.loadMessagesFromServer();
+    setInterval(this.loadMessagesFromServer(), 2000);
+  },
   render: function(){
     return (
       <div className='chatbox'>
-        <ChatBox />
+        <ChatBox messageData={this.state.messageData}/>
         <UserList />
-        <FormBox />
+        <FormBox onMessageSubmit={this.handleMessageSubmit}/>
       </div>
     );
   }
 });
 var ChatBox = React.createClass({
   render: function(){
+    var messageNode = this.props.messageData.map(function(message){
+      return(
+        <Message user={message.user} key={message.id} text={message.text}>
+        </Message>
+      );
+    });
     return(
       <div className='chatbox__messages'>
-        <Message />
-        <Message />
+        {messageNode}
       </div>
     );
   }
@@ -34,48 +85,101 @@ var Message = React.createClass({
     return(
     <div className='chatbox__messages__user-message'>
       <div className='chatbox__messages__user-message--text'>
-        <p className='name'> Jered </p>
-        <p className='message-text'> Hello, world. This is my chat. </p>
+        <p className='name'> {this.props.user} </p>
+        <p className='message-text'> {this.props.text}</p>
       </div>
     </div>
     );
   }
 });
 ////
+
+  
 var UserList = React.createClass({
   render: function(){
     return(
       <div className='chatbox__user-list'>
         <h1> User List </h1>
-        <div className='chatbox__user--active'>
-          <p>Jack Thomson</p>
-        </div>
-        <div className='chatbox__user--busy'>
-          <p>Angelina Jolie</p>
-        </div>
+        <User user='Angelina Morris'/>
+        <User user='Jacob Smith' />
       </div>
     );
   }
 });
 ////UserBox components
 
+var User = React.createClass({
+
+  getInitialState: function(){
+    return({userStatus: 0});
+  },  
+  render: function(){
+    var userStatusClass = 'chatbox__user'
+    switch(this.state.userStatus){
+    case 0: 
+      userStatusClass += '--active';
+      break;
+    case 1: 
+      userStatusClass += '--busy';
+      break;
+    case 2:
+      userStatusClass += '--away';
+      break;
+    }
+    return(
+      <div className={userStatusClass}>
+      <p> {this.props.user} </p>
+      </div>
+    );
+  }
+});
 ////
 var FormBox = React.createClass({
+  getInitialState: function(){
+    return({text: ''});
+  },
+  handleMessageChange: function(e){
+    this.setState({text: e.target.value});
+  },
+  handleSumbit: function(e){
+    e.preventDefault();
+    var text = this.state.text.trim();
+    if(!text){
+      return;
+    }
+    this.props.onMessageSubmit({user: 'Jered', text: text});
+    this.setState({text: ''});
+  },
   render: function(){
     return(
       <div className='FormBox'>
-        <form>
-          <input type='text'  placeholder='Enter your message here:'/>
+        <form onSubmit={this.handleSumbit}>
+          <input 
+        type='text'  
+        placeholder='Enter your message here:' 
+        value={this.state.text} 
+        onChange={this.handleMessageChange}
+      />
         </form>
       </div>
     );
   }
 });
-////InputBox components
+////FormBox components
+
+/*var FormInput = React.createClass({
+
+
+  render: function(){
+    return(
+      
+    );  
+  }  
+});*/
 
 ////
 
 ReactDOM.render(
-  <Chat />,
+  <Chat messageURL='api/messages'/>,
   document.getElementById('content')
 );
--- server.js
+++ server.js
@@ -5,11 +5,46 @@ var fs            = require('fs'),
     bodyParser    = require('body-parser');
 var app = express();
 
-var MESSAGE_FILE = path.join(__dirname, 'chat.json');
+var MESSAGE_FILE = path.join(__dirname, 'messages.json');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/messages', function(req, res){
+  fs.readFile(MESSAGE_FILE, function(err, data){
+    if(err){
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/messages', function(req, res){
+  fs.readFile(MESSAGE_FILE, function(err, data){
+    if(err){
+      console.error(err);
+      process.exit(1);
+    }
+    var messages = JSON.parse(data);
+    var newMessage = {
+      id: Date.now(),
+      user: req.body.user,
+      text: req.body.text
+    };
+    messages.push(newMessage);
+    fs.writeFile(MESSAGE_FILE, JSON.stringify(messages, null, 4), function(err){
+      if(err){
+        console.error(err);
+        process.exit(1);
+      }
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(messages);
+    });
+  });
+});
 
 
 app.listen(process.env.PORT || 3000, function(){
